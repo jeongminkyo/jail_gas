3931bd6fc9b0b642029ec3849d1bbaa08655a390
minkyojeong <redwonder@naver.com>
2018-10-05 18:45:13 +0900

    deploy 변경

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, 'https://github.com/jeongminkyo/jail_gas.git'
 set :deploy_to, '/var/www/jail_gas'
 set :bundle_flags, ""
 set :use_sudo, true
+set :rbenv_ruby, '2.2.4'
 
 set :passenger_restart_with_sudo, true
 set :passenger_restart_with_touch, true
@@ -14,6 +15,7 @@ set :passenger_restart_options,
     -> { "#{deploy_to} --ignore-app-not-running --rolling-restart" }
 
 set :linked_dirs, %w{log}
+append :linked_files, "config/application.yml"
 
 namespace :deploy do
   desc 'Restart application'
